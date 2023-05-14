55628d36a75e1c50896c9f73fab474d077720d70
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-05-14 12:26:36 -0700

    got install prompt to work

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,8 +1,6 @@
 // Caching js and css requires workbox-strategies to be installed
 // To actually respond to requests with a cached response, we need to use a strategy called StaleWhileRevalidate
 // This strategy will first check the cache for a response, and if it finds one, it will return it.
-
-
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,22 +4,22 @@ const textHeader = document.getElementById('textHeader');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    installBtn.style.visibility = 'visible';
-    textHeader.textContent = 'Click the button to install!';
-    
-    // TODO: Implement a click event handler on the `butInstall` element
-    butInstall.addEventListener('click', () => {
-      event.prompt();
-      butInstall.setAttribute('disabled', true);
-      butInstall.textContent = 'Installed!';
-    });
-  });
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
-
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    textHeader.textContent = 'Successfully installed JATE!';
-    console.log('🥳', 'appinstalled', event);
-  });
+    window.deferredPrompt = null;
+});
