cf3a21ea33e1e3d1f245d6583f5a1a63703207b3
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-05-13 23:50:38 -0700

    still attempt to debug why install wont work

--- client/src/sw.js
+++ client/src/sw.js
@@ -4,7 +4,7 @@
 
 
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
