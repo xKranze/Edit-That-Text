24ae2bf744b9a0048fb71ef2296d5407c2ca4f2c
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-05-16 14:36:58 -0700

    fixed database.js result.value

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -37,7 +37,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 
